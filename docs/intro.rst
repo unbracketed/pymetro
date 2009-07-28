191f9491b2b169a181ba3e65e9d65f5c31d7cd19
Brian Luft <brian@unbrackted.org>
2009-07-27 18:26:36 -0700

    refactors initial code to make the use a little more pythonic

--- docs/intro.rst
+++ docs/intro.rst
@@ -7,4 +7,4 @@ with the API in a Pythonic manner: results are returned as Python objects with s
 for the familiar notions of working with data structures.
 
 The Metro Developer API provides access to public transit routes and schedules for
-carrier agencies across Southern California. 
+carrier agencies across Southern California.  Please be sure to review and respect the `Metro Developer API Terms and Conditions. <http://developer.metro.net/policies/terms/>`_
--- metro/api.py
+++ metro/api.py
@@ -1,56 +1,71 @@
 from urllib2 import urlopen
-from xml.etree.ElementTree import ElementTree
+try:
+    from  xml.etree.ElementTree import ElementTree as ET # Python >= 2.5
+except ImportError:
+    try:
+        from elementtree.ElementTree import ElementTree as ET
+    except ImportError:
+        raise Exception("ElementTree is required")
+    
 from constants import METRO_API_URL,CARRIERS_ENDPOINT
 
-class Metro(object):
-    """
-    The object responsible for communicating with the Metro API
-    """
+class Service(object):
+    """A utility class for handling calls to the webservice and returning the response as an ElementTree"""
+
+    def get_service_data(self,service,api_key):
+        """"Makes a call to a webservice endpoint and returns the result as an ElementTree"""
+        response = urlopen("%s/%s?api_key=%s" % (METRO_API_URL,service,api_key))
+        et = ET()
+        et.parse(response)
+        return et
+
+class Carriers(Service):
+    """The list of all Metro passenger carrier agencies"""
     
-    def __init__(self,api_key):
-        self.api_key = api_key
-        
-    def _get_url(self,service):
-        response = urlopen("%s/%s?api_key=%s" % (METRO_API_URL,service,self.api_key))
-        
-        return response
-        
-    def _get_service_data(self,service):
-        cache_name = '%s_cache' % service.replace('.','').replace('/','')
-        if hasattr(self,cache_name):
-            print 'returning from cache'
-            return getattr(self,cache_name)
-        else:
-            response = self._get_url(service)
-            et = ElementTree()
-            et.parse(response)
-            setattr(self,cache_name,et)
-            return et
-        
-    def carriers(self,id=None,code=None):
-        results = []
-        response = self._get_service_data(CARRIERS_ENDPOINT)
-        
-        carriers = response.find('carriers')
-        for carrier in carriers.findall('item'):
-            item_id = carrier.find('id').text
-            item_text = carrier.find('text').text
-            item_code = carrier.find('code').text
-            c = Carrier(item_id,item_text,item_code)
-            if id and item_id == str(id):
-                return c
-            elif code and code == item_code:
-                return c
-            else:
-                results.append(c)
-        return results
+    def __get__(self,obj,objtype):
+        if not hasattr(self,'_items'):
+            data = self.get_service_data(CARRIERS_ENDPOINT,obj.api_key)
+            carriers = data.find('carriers')
+            results = []
+            for carrier in carriers.findall('item'):
+                item_id = carrier.find('id').text
+                item_text = carrier.find('text').text
+                item_code = carrier.find('code').text
+                results.append(Carrier(int(item_id),item_text,item_code))
+            setattr(self,'_items',results)
+        return self
     
-    def routes(self,carrier):
+    def __repr__(self):
+        return repr(self._items)
+    def __len__(self):
+        return len(self.get())
+    def __getitem__(self,k):
+        return self._items[k]
+    def __contains__(self,elt):
+        return elt in self._items
+    
+    def get(self,id=None,code=None):
         """
-        Lookup the routes for a Metro carrier
+        Used for selecting specific Carrier instances. It is assumed that both
+        id and code are unique. Only one of the keyword arguments will be
+        used in the lookup.
+        
+        id  -  The carrier ID
+        code - A two-letter code associated with the carrier
+        
+        If no parameters are supplied, the entire list is returned.
+        If no Carrier is found matching the search criteria, None is returned.
         """
-        pass
-    
+        if id is None and code is None:
+            return self._items if len(self._items) else []
+        for carrier in self._items:
+            if id and carrier.id == id:
+                return carrier
+            elif code and carrier.code == code:
+                return carrier
+        return None
+            
+
 class Carrier(object):
     """
     Represents a carrier
@@ -61,22 +76,31 @@ class Carrier(object):
         self.name = name
         self.code = code
         
+    routes = Routes()
+    
+    def __repr__(self):
+        return "<Carrier: %s>" % self.name
     def __unicode__(self):
         return u"%s:%s:%s" % (self.id,self.name,self.code)
-        
     def __str__(self):
         return self.__unicode__()
 
-class Route(object):
+class Routes(object):
     """
     http://developer.metro.net/docs/routes/
     """
     pass
+
+class Route(object):
+    pass
+
+
+class MetroAPI(object):
+    """
+    The object responsible for communicating with the Metro API
+    """
+    
+    def __init__(self,api_key):
+        self.api_key = api_key
         
-if __name__ == '__main__':
-    metro = Metro('55bea475-703c-493e-8586-7c00e48b2b1b')
-    carriers = metro.carriers()
-    for carrier in carriers:
-        print carrier
-    foothill = metro.carriers(id=23)
-    print foothill.name
+    carriers = Carriers()    
